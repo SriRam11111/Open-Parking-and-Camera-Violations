034295a966f21812c5f015337ccba111d9b37ebb
SimpliLearn GitHub <ramkumar.p@simplilearn.net>
2023-01-11 11:52:21 +0530

    used table-data-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
-
+import axios from "axios";
+import data from '../src/data/data.json'
+import Show_Data from './components/show_data';
+import { useEffect, useState } from 'react';
 function App() {
+  const[Data,SetData]=useState([])
+  const getData = async () =>{
+    try{
+      const response =await axios.get("https://data.cityofnewyork.us/resource/nc67-uf89.json");
+      SetData(response.data);
+    }catch(error){
+      console.log(error);
+    }
+  };
+
+  useEffect(()=>{
+    getData();
+  },[])
   return (
-    <div className="App">
-  
+    <div >
+       <Show_Data  data={data}/>
     </div>
   );
 }
--- src/components/show_data.js
+++ src/components/show_data.js
@@ -1,10 +1,49 @@
-import React from 'react'
-
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import DataTable, { Selector } from 'react-data-table-component';
 function Show_Data() {
-  return (
-    <div>
-      
-    </div>
+  const[Data,SetData]=useState([])
+  const getData = async () =>{
+    try{
+      const response =await axios.get("https://data.cityofnewyork.us/resource/nc67-uf89.json");
+      SetData(response.data);
+    }catch(error){
+      console.log(error);
+    } 
+  };
+  const columns=[
+    {
+      name:"plate number",
+      selector:(row)=>row.plate,
+    },
+    {
+      name:"state",
+      selector:(row)=>row.state,
+    },
+    {
+      name:"summons number",
+      selector:(row)=>row.summons_number,
+    },
+    {
+      name:"fine_amount",
+      selector:(row)=>row.fine_amount,
+    },
+    {
+      name:"violatiiiion",
+      selector:(row)=>row.violation,
+    }
+  ]
+
+  useEffect(()=>{
+    getData();
+  },[])
+  return(
+    <>
+    {/* {console.log(props.data)} */}
+    <DataTable title="Open Parking and Camera Violations" columns={columns} data={Data} pagination/>
+    </>
+
   )
 }
 
